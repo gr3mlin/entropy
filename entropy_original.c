b4c04c577e3ccb81ac99d8399029ed9ce9a9414c
Gr3mlin <mweigand@gmail.com>
2015-02-24 19:58:32 -0500

    Fixed driveGnuPlots to output only points, not lines. Updated README, fixed comments on several files

---
+++ entropy_original.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+ 
+#define MAXLEN 100 //maximum string length
+ 
+int makehist(char *S,int *hist,int len){
+	int wherechar[256];
+	int i,histlen;
+	histlen=0;
+	for(i=0;i<256;i++)wherechar[i]=-1;
+	for(i=0;i<len;i++){
+		if(wherechar[(int)S[i]]==-1){
+			wherechar[(int)S[i]]=histlen;
+			histlen++;
+		}
+		hist[wherechar[(int)S[i]]]++;
+	}
+	return histlen;
+}
+ 
+double entropy(int *hist,int histlen,int len){
+	int i;
+	double H;
+	H=0;
+	for(i=0;i<histlen;i++){
+		H-=(double)hist[i]/len*log2((double)hist[i]/len);
+	}
+	return H;
+}
+ 
+int main(void){
+	//char S[MAXLEN];
+	int len,*hist,histlen;
+	double H;
+	int i;
+	//scanf("%[^\n]",S);
+	char S[] = {1,2,2,3,3,3,4,4,4,4};
+	S[strlen(S)-1]= NULL;
+	for(i=0;i<strlen(S);i++){
+		printf("%d ",S[i]);
+	}
+	//printf("scanf String length: %d\n",histlen);
+	len=strlen(S);
+	hist=(int*)calloc(len,sizeof(int));
+	histlen=makehist(S,hist,len);
+	//hist now has no order (known to the program) but that doesn't matter
+	H=entropy(hist,histlen,len);
+	printf("%lf\n",H);
+	return 0;
+}
